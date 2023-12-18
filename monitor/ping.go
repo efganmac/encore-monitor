f36cf9c9203d289cc7313cbf57621b93fca9e4d3
efekanbicermac <efekan.bicer8@gmail.com>
2023-12-18 17:04:25 +0300

    Create API endpoint

--- monitor/ping.go
+++ monitor/ping.go
@@ -1,1 +1,32 @@
 package monitor
+
+import (
+	"context"
+	"net/http"
+	"strings"
+)
+
+type PingResponse struct {
+	Up bool `json:"up"`
+}
+
+//encore:api public path=/ping/*url
+func Ping(ctx context.Context, url string) (*PingResponse, error) {
+
+	if !strings.HasPrefix(url, "http:") && !strings.HasPrefix(url, "https:") {
+		url = "https://" + url
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return &PingResponse{Up: false}, nil
+	}
+	resp.Body.Close()
+
+	up := resp.StatusCode < 400
+	return &PingResponse{Up: up}, nil
+}
